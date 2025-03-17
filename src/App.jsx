672581ba8d3a48af419865ba75f5153abf51ed3b
ravishka1325 <ravishka.1325@gmail.com>
2025-03-17 11:05:26 +0530

    homePage hero layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import HomePage from './pages/homePage'
+import './App.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <HomePage/>
+    </>
   )
 }
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import mainImage from '../assets/mainImage.png'
+import './pageStyling/HomePage.css'
+<style>@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');</style>
+
+
+const HomePage = () => {
+  return (
+  <>
+    <div className="SectionOne">
+     <div className="mainText">
+      <h1>We Crush Your Competitors, Goals, And Sales Records - Without The B.S.</h1>
+      <button className='mainButton'> GET FREE CONSULTATION</button>
+     </div>
+
+    </div>
+  </>
+  )
+}
+
+export default HomePage
