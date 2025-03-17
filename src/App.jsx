6b9fced6b03b3157fd71bad4cf213e24d37adfc4
ravishka1325 <ravishka.1325@gmail.com>
2025-03-18 01:47:28 +0530

    Rename HomePage to MainPage and add corresponding styles; implement layout and animations for improved user experience

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import './App.css';
 import BootstrapNav from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import MaintenancePage from './components/Maintenance/MaintenancePage'
-import HomePage from './pages/HomePage';
+import HomePage from './pages/MainPage';
+
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
 </style>
