d764bb51f25a4e5c2501d71ec5290281b5cb89e9
ravishka1325 <ravishka.1325@gmail.com>
2025-03-17 20:42:49 +0530

    Refactor HomePage layout and styles; integrate Bootstrap Navbar and enhance responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomePage from './pages/homePage'
 import './App.css'
+import BootstrapNav from './components/Navbar/Navbar'
 
 const App = () => {
   return (
     <>
+    <BootstrapNav/>
     <HomePage/>
     </>
   )
